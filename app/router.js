dfc051e4d1ae34de46d999a386d033465c4d1618
Krzysztof Pająk <chris.araneo@gmail.com>
2025-07-22 18:31:16 +0200

    feat: init Ember.js project

---
+++ app/router.js
@@ -0,0 +1,9 @@
+import EmberRouter from '@ember/routing/router';
+import config from 'push-up-trainer/config/environment';
+
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
+
+Router.map(function () {});
---
+++ tests/test-helper.js
@@ -0,0 +1,14 @@
+import Application from 'push-up-trainer/app';
+import config from 'push-up-trainer/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { loadTests } from 'ember-qunit/test-loader';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+setupEmberOnerrorValidation();
+loadTests();
+start();
