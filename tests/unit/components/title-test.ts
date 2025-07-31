673e9242ad6c324084a7166a3fc791edd244c535
Krzysztof Pająk <chris.araneo@gmail.com>
2025-08-01 00:00:59 +0200

    test(title): add tests

---
+++ tests/unit/components/title-test.ts
@@ -0,0 +1,10 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'push-up-helper/tests/helpers';
+
+module('Unit | Component | title', function (hooks) {
+  setupTest(hooks);
+
+  test('it exists as a component', function (assert) {
+    assert.ok(true, 'Title component is available');
+  });
+});
