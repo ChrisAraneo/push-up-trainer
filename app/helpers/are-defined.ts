3c6d23a9ec4b15ab008b3da4fbb92427dee19118
Krzysztof Pająk <chris.araneo@gmail.com>
2025-08-01 18:48:24 +0200

    feat(are-defined): create util function

---
+++ app/helpers/are-defined.ts
@@ -0,0 +1,7 @@
+import { isUndefined } from 'lodash';
+
+export const areDefined = <T>(values: T[]): boolean => {
+  return values.every((value): boolean => {
+    return !isUndefined(value);
+  });
+}
