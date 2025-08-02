25d456f1684e80c1a584673f27644e4f965bde03
Krzysztof Pająk <chris.araneo@gmail.com>
2025-08-02 22:22:53 +0200

    refactor: delete areDefined util function

--- app/helpers/are-defined.ts
+++
@@ -1,7 +0,0 @@
-import { isUndefined } from 'lodash';
-
-export const areDefined = <T>(values: T[]): boolean => {
-  return values.every((value): boolean => {
-    return !isUndefined(value);
-  });
-};
