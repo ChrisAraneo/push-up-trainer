944fa71ed08f2be121254a3ef20962fcb3aa7812
Krzysztof Pająk <chris.araneo@gmail.com>
2025-08-03 15:26:00 +0200

    feat(sound-player): create class

---
+++ app/utils/sound-player.ts
@@ -0,0 +1,43 @@
+export class SoundPlayer {
+  private audios: Map<string, HTMLAudioElement> = new Map();
+  private audio: HTMLAudioElement | null = null;
+
+  load(path: string): void {
+    const audio = new Audio(path);
+    audio.volume = 0.7;
+    this.audios.set(path, audio);
+  }
+
+  play(path: string): void {
+    try {
+      if (this.audio) {
+        this.stop();
+      }
+
+      if (!this.audios.has(path)) {
+        this.load(path);
+      }
+
+      this.audio = this.audios.get(path)!;
+      const promise = this.audio.play();
+
+      if (promise !== undefined) {
+        promise.catch(error => {
+          console.warn('Unable to play sound', error);
+        });
+      }
+    } catch (error) {
+      console.warn('Error loading or playing sound', error);
+    }
+  }
+
+  stop(): void {
+    if (this.audio) {
+      this.audio.pause();
+      this.audio.currentTime = 0;
+      this.audio = null;
+    }
+  }
+}
+
+export const soundPlayer = new SoundPlayer();
