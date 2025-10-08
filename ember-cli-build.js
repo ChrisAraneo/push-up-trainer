726a686435268091ceb3b940b38ce95848d39201
Krzysztof Pająk <chris.araneo@gmail.com>
2025-10-08 21:08:50 +0200

    fix(gh-pages): update fingerprint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,14 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    'ember-cli-babel': { enableTypeScriptTransform: true },
-    // Disable SRI for GitHub Pages
     SRI: {
       enabled: false,
     },
     fingerprint: {
       enabled: true,
+      prepend: 'https://chrisaraneo.github.io/push-up-trainer/',
     },
+    'ember-cli-babel': { enableTypeScriptTransform: true },
   });
 
   return app.toTree();
